68d34625348ce141d72c13a0ca92eb6f9e7f37a6
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-02-07 11:42:06 -0600

    Add an interactive version of API mockup

---
+++ api_docs/mockup_frontend/demo.py
@@ -0,0 +1,16 @@
+def get_it(name):
+    return {
+        'get': name
+    }
+
+def getRecipes():
+    return {}
+
+def getRecipeByID():
+    return {}
+
+def updateRecipeByID():
+    return {}
+
+def copyRecipe():
+    return {}
